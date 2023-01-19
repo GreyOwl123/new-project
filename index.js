026cca2c0b6cbc9ae7d52a3ef39d12be55c842b0
Muhammed Ibrahim <muib13579@gmail.com>
2023-01-19 15:38:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// Load HTTP module
+const http = require("http");
+const fs = require("fs");
+
+const hostname = "localhost"
+const port = 3000;
+
+const server = http.createServer(function (req, res) {
+   if(req.url ==='/'){
+    fs.readFile('index.html', function(err,data){
+      if(err) throw err;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(data);
+  return res.end;
+    });
+  }
+
+  else if (req.url ==='/about'){
+    fs.readFile('about.html', function(err,data){
+      if(err) throw err;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(data);
+  return res.end;
+    });
+  }
+
+  else if (req.url ==='/contact-me'){
+    fs.readFile('contact-me.html', function(err,data){
+      if(err) throw err;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(data);
+  return res.end;
+    });
+  }
+
+  else {
+    fs.readFile('404.html', function(err,data){
+      if(err) throw err;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(data);
+  return res.end;
+    });
+  }
+});
+
+server.listen(port, hostname, function () {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
